06fdb11d44f91200bd281540977d8c5252de4bb4
jultrishyyy <jultrishyyy@gmail.com>
2025-09-23 15:47:33 +0100

    Update __init__.py

--- algorithms/sinkhorn/__init__.py
+++ algorithms/sinkhorn/__init__.py
@@ -1,10 +1,4 @@
 from sinkhorn import Sinkhorn
-from .prob_sinkhorn import ProbSinkhorn
-from .penalty_sinkhorn import PenaltySinkhorn
-from .lassosinkhorn import LassoSinkhorn
-from .l1_sinkhorn import L1Sinkhorn
-from .recover_SWaT import recover_SWaT
-from .kl_sinkhorn import KLSinkhorn
 
 __all__ = [
     "Sinkhorn",
